a761be46533f9bee5c6021050c1f9ffc0306c053
enchenghu <shuxin1638@qq.com>
2022-10-08 11:22:10 +0800

    remove useless

--- build/tt/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/encheng/catkin_ws/tcp_server/src/tt
-# Build directory: /home/encheng/catkin_ws/tcp_server/build/tt
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
