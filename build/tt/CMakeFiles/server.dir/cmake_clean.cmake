e2b738963738631a4571265e5d7bbf0cd6e05601
enchenghu <shuxin1638@qq.com>
2022-10-08 11:18:06 +0800

    add gitignore

---
+++ build/tt/CMakeFiles/server.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "/home/encheng/catkin_ws/tcp_server/devel/lib/tt/server"
+  "/home/encheng/catkin_ws/tcp_server/devel/lib/tt/server.pdb"
+  "CMakeFiles/server.dir/src/server.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/server.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/tt/CMakeFiles/talker_node.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "/home/encheng/catkin_ws/tcp_server/devel/lib/tt/talker_node"
+  "/home/encheng/catkin_ws/tcp_server/devel/lib/tt/talker_node.pdb"
+  "CMakeFiles/talker_node.dir/src/talker.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/talker_node.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
